2a354688682c240f6c4c2657612fa855e6906cae
Evgeniy <Test@neto.com>
2024-12-14 09:30:12 +0300

    load all files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,7 +10,13 @@ func main() {
 
 	//taskOne()
 
-	taskTwo()
+	//taskTwo()
+
+	//taskThree()
+
+	//taskFour()
+
+	taskFive()
 
 }
 
@@ -53,8 +60,10 @@ func taskTwo() {
 	operStr, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
-	}*/
-	//fmt.Println("Вы ввели:", operStr)
+	}
+	fmt.Println("Вы ввели:", operStr)  */
+	// Функция не видит входящий string
+
 	operStr = "сложение"
 	fmt.Println(Calculate(x, y, operStr))
 	operStr = "вычитание"
@@ -63,28 +72,94 @@ func taskTwo() {
 	fmt.Println(Calculate(x, y, operStr))
 	operStr = "деление"
 	fmt.Println(Calculate(x, y, operStr))
-	//fmt.Println("Значение переменной b - " + strconv.Itoa(b))
-	//fmt.Println("Значение переменной c - " + strconv.Itoa(c))
+
 }
 
-func Calculate(x, y float64, operation string) float64 { // taskTwo
+func Calculate(x, y float64, operation string) string { // taskTwo
 
 	if y == 0 {
-		return 0
-		//Println("Делить на ноль нельзя!"))
+		//return 0
+		return fmt.Sprint("Делить на ноль нельзя!")
 	}
 
 	switch operation {
 	case "сложение":
-		return x + y
+		return fmt.Sprintf("сложение чисел %.2f и %.2f равно %.2f", x, y, x+y)
 	case "вычитание":
-		return x - y
+		return fmt.Sprintf("разница чисел %.2f и %.2f равно %.2f", x, y, x-y)
 	case "умножение":
-		return x * y
+		return fmt.Sprintf("умножение чисел %.2f и %.2f равно %.2f", x, y, x*y)
 	case "деление":
-		return x / y
+		return fmt.Sprintf("деление чисел %.2f и %.2f равно %.2f", x, y, x/y)
 	default:
-		return 0
+		return fmt.Sprintf("%.2f", 0)
+	}
+}
+
+/*
+3. Работа с дробными числами и округлением
+Программа принимает два числа типа float64 — длину и ширину прямоугольника.
+Вычисляет площадь и периметр прямоугольника. Результаты округляются до двух
+знаков после запятой.
+*/
+
+func taskThree() {
+
+	var length, width float64
+
+	fmt.Print("Введите значение длины прямоугольника length: ")
+	fmt.Scan(&length)
+	fmt.Print("Введите значение ширины прямоугольника width: ")
+	fmt.Scan(&width)
+
+	fmt.Printf("Периметр прямоугльника = %.2f\n", 2*(length+width))
+	fmt.Printf("Площадь прямоугльника = %.2f\n", length*width)
+
+}
+
+/*=========================================================================================*/
+/* 4. Приведение типов
+Напишите программу, которая запрашивает у пользователя целое число, переводит его в float64,
+делит на 3 и преобразует результат обратно в int. Выведите результат преобразования.*/
+/*=========================================================================================*/
+func taskFour() {
+	var x, result int
+
+	fmt.Print("Введите значение переменной x: ")
+
+	_, err := fmt.Scan(&x)
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+	} else {
+		fmt.Println("Вы ввели:", x)
 	}
 
+	result = int(float64(x) / 3)
+
+	fmt.Println("Результат преобразования -", result)
+}
+
+/*=========================================================================================*/
+/* 5. Работа с функциями и ошибками
+Создайте функцию SquareRoot, которая принимает число типа float64 и возвращает
+квадратный корень числа. Если передано отрицательное число, функция должна возвращать ошибку.*/
+/*=========================================================================================*/
+func taskFive() {
+	var x float64
+
+	fmt.Print("Введите значение переменной x: ")
+
+	_, err := fmt.Scan(&x)
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+	} else if x < 0 {
+		fmt.Println("Ошибка ввода: отрицательное число")
+	} else {
+		fmt.Println("Квадратный корень числа", x, "=", SquareRoot(x))
+
+	}
+}
+
+func SquareRoot(x float64) float64 {
+	return math.Sqrt(x)
 }
