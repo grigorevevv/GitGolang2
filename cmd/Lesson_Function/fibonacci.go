10200894abcb444fbb91b74c39a42afcd3ef5b44
Evgeniy <Test@neto.com>
2025-01-02 18:19:41 +0300

    func fibonacci second

--- cmd/Lesson_Function/fibonacci.go
+++ cmd/Lesson_Function/fibonacci.go
@@ -4,13 +4,6 @@ func fibonacci(n int) int {
 
 	sliceFib := []int{0, 1}
 	appNum := 0
-	/*
-		sliceFib = append(sliceFib, 0)
-		sliceFib = append(sliceFib, 1)
-
-		appNum := sliceFib[0] + sliceFib[1]
-		sliceFib = append(sliceFib, appNum)
-	*/
 
 	for i := 2; i <= n; i++ {
 		appNum = sliceFib[i-1] + sliceFib[i-2]
