d1e47803f2c91f9eb63bfce0c846d1c6e8064077
Evgeniy <Test@neto.com>
2025-02-02 18:39:53 +0300

    edit four task

--- cmd/10_lesson_floating-point/main.go
+++ cmd/10_lesson_floating-point/main.go
@@ -1,51 +1,59 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
 	sliceNumber := []int{3, 5, -10, 15, 17, 0, -21, 0, 1, 1, 1, 15, 7, 7, 7, 13, 7, 7, 12, 14, 16}
 
-	fmt.Println("Задача 1: Проверка делимости числа")
-	for _, number := range sliceNumber {
-		n, err := checkinForDivision(number)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		fmt.Printf("Число %d %s\n", number, n)
-	}
-	fmt.Println()
-
-	fmt.Println("Задача 2: Проверка диапазона")
-	for _, number := range sliceNumber {
-		fmt.Printf("Число %d %s\n", number, rangeChek(number))
-	}
-	fmt.Println()
-
-	fmt.Println("Задача 3: Вычисление среднего")
-
-	for i := 0; i < len(sliceNumber)-2; {
-		n1, n2, n3 := sliceNumber[i], sliceNumber[i+1], sliceNumber[i+2] //??????? :=
-		fmt.Printf("Из чисел %d,%d,%d - %s\n", n1, n2, n3, averageCalculate(n1, n2, n3))
-		i += 3
-	}
-	fmt.Println()
+	//fmt.Println("Задача 1: Проверка делимости числа")
+	//for _, number := range sliceNumber {
+	//	//n, err := checkDivision(number)
+	//	//if err != nil {
+	//	//	fmt.Println(err)
+	//	//	continue
+	//	//}
+	//	fmt.Printf("Число %d %s\n", number, checkDivision(number))
+	//}
+	//fmt.Println()
+	//
+	//fmt.Println("Задача 2: Проверка диапазона")
+	//for _, number := range sliceNumber {
+	//	fmt.Printf("Число %d %s\n", number, rangeCheck(number))
+	//}
+	//fmt.Println()
+	//
+	//fmt.Println("Задача 3: Вычисление среднего")
+	//
+	//for i := 0; i < len(sliceNumber)-2; i += 3 {
+	//	n1, n2, n3 := sliceNumber[i], sliceNumber[i+1], sliceNumber[i+2]
+	//	fmt.Printf("Из чисел %d,%d,%d - %s\n", n1, n2, n3, averageCalculate(n1, n2, n3))
+	//}
+	//fmt.Println()
 
 	fmt.Println("Задача 4: Подсчёт простых чисел")
 
-	sliceTwo := []int{1}
+	sliceTwo := []int{2, 3, 5, 7}
 	sliceThree := []int{}
+	//fmt.Println(amountNumbers(sliceNumber))
+	//fmt.Println(amountNumbers(sliceTwo))
+	//fmt.Println(amountNumbers(sliceThree))
+
+	fmt.Println(sliceNumber)
 	fmt.Println(amountNumbers(sliceNumber))
 	fmt.Println(amountNumbers(sliceTwo))
-	fmt.Println(amountNumbers(sliceThree))
 
+	for i := 2; i <= 10; i++ {
+		sliceThree = append(sliceThree, i)
+	}
+	fmt.Println(amountNumbers(sliceThree))
+	sliceFour := []int{4, 6, 8}
+	fmt.Println(amountNumbers(sliceFour))
 	fmt.Println()
 }
 
-func checkinForDivision(dividend int) (string, error) {
+func checkDivision(dividend int) string {
 	/*   Напишите программу, которая принимает целое число и проверяет:
 	1. Делится ли оно на 3;
 	2. Делится ли оно на 5;
@@ -55,38 +63,36 @@ func checkinForDivision(dividend int) (string, error) {
 	Если ни на одно из чисел:                                     Число не делится на 3 и 5.
 	Подсказка: вспоминаем операцию "%"
 	*/
-	if dividend == 0 {
-		return "", errors.New("0 не делится!")
-	}
+	//if dividend == 0 {
+	//	return "", errors.New("0 не делится!")
+	//}
 
 	if dividend%3 == 0 && dividend%5 == 0 {
-		return "делится на 3 и 5", nil
+		return "делится на 3 и 5"
 	} else if dividend%3 == 0 && dividend%5 != 0 {
-		return "Делится на 3", nil
+		return "Делится на 3"
 	} else if dividend%3 != 0 && dividend%5 == 0 {
-		return "Делится на 5", nil
+		return "Делится на 5"
 	} else {
-		return "не делится на 3 и 5", nil
+		return "не делится на 3 и 5"
 	}
-	return "", nil
+	return ""
 }
 
-func rangeChek(number int) string {
+func rangeCheck(number int) string {
 	/*   Напишите программу, которая принимает на вход целое число и проверяет:
 	1. Попадает ли оно в диапазон от -10 до 10 включительно.
 	2. Если число больше 10, выведите сообщение:     	Число больше 10.
 	3. Если число меньше -10, выведите сообщение:    	Число меньше -10.
 	4. Если число в диапазоне от -10 до 10, выведите:	Число находится в пределах [-10, 10].
 	*/
-
 	if number >= -10 && number <= 10 {
 		return "находится в пределах [-10, 10]"
-	} else if number < -10 {
-		return "меньше -10"
-	} else {
-		return "больше 10"
 	}
-	return ""
+	if number < -10 {
+		return "меньше -10"
+	}
+	return "больше 10"
 }
 
 func averageCalculate(numberOne, numbertwo, numberThree int) string {
@@ -113,16 +119,36 @@ func amountNumbers(sliceInt []int) string {
 	Пример вывода: 		Простые числа: 2, 3, 5, 7
 						Количество простых чисел: 4.
 	*/
-	if len(sliceInt) < 2 {
-		return "Простых чисел нет."
-	}
 
-	result := "Простые числа: "
+	result := ""
+	counter := 0
 	for _, number := range sliceInt {
-		result += fmt.Sprintf("%d,", number)
-	}
-	result = result[:len(result)-1] //?????
-	result += fmt.Sprintf("\nКоличество простых чисел: %d", len(sliceInt))
-	return result
 
+		if primeNumberBool(number) {
+			result += fmt.Sprintf("%d - Простое число\n", number)
+			counter++
+		} else {
+			result += fmt.Sprintf("%d - Не простое число\n", number)
+		}
+
+	}
+	if counter > 0 {
+		result += fmt.Sprintf("Количество простых чисел - %d\n", counter)
+	} else {
+		result += "Простых чисел нет"
+	}
+
+	return result
+}
+
+func primeNumberBool(number int) bool {
+	if number < 2 {
+		return false
+	}
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
 }
